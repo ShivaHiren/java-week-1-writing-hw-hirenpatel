a9c8b5f0538c17a6c6f82fabb3a19a1aee671dd0
Shivahiren <shivahiren108@gmail.com>
2022-10-13 23:14:00 +0100

    java-week-1-writing-mw-hirenpatel

---
+++ src/javaweek1writinghwhirenpatel/Programme_7_SumOfTwoNumbers.java
@@ -0,0 +1,15 @@
+package javaweek1writinghwhirenpatel;
+/* Write a Java program to print the sum of two numbers.
+ Test Data: 74 + 36
+ Expected Output: 110 */
+
+public class Programme_7_SumOfTwoNumbers {
+    public static void main(String[] args) {
+        //The two integers are value and value
+        int value1 =74;
+        int value2 =36;
+        int sum = value1 + value2;
+        //concatention
+        System.out.println("The Sum of " +value1+" and " +value2+  " is " + sum);
+    }
+}
